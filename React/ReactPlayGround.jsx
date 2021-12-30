145c2d69641592b975db56566a734782b465300c
rayct <rayct@icloud.com>
2021-12-30 01:56:49 +0000

    React Course Update - 11/47

--- React/ReactPlayGround.jsx
+++ React/ReactPlayGround.jsx
@@ -203,7 +203,45 @@ class TypesOfFood extends React.Component {
 
 
 
-// 11./47
+// 11./47 Compose React Components
+
+// TASK: Nest two components inside of Fruits — first NonCitrus, and then Citrus.
+// Both of these components are provided for you behind the scenes.Next,
+// nest the Fruits class component into the TypesOfFood component, below the h1 heading element and above Vegetables.
+// The result should be a series of nested components, which uses two different component types.
+class Fruits extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Fruits:</h2>
+        { /* Change code below this line */ }
+
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+     super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        { /* Change code below this line */ }
+
+        { /* Change code above this line */ }
+        <Vegetables />
+      </div>
+    );
+  }
+};
+
 
 // 12./47
 
