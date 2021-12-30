4aa08f0bd902e459f8044a73ff9e6c3f70354cfa
rayct <rayct@icloud.com>
2021-12-30 01:52:18 +0000

    React Course Update - 10/47 Completed

--- React/ReactPlayGround.jsx
+++ React/ReactPlayGround.jsx
@@ -157,7 +157,51 @@ class ParentComponent extends React.Component {
 
 
 
-// 10./47
+// 10./47 Use React to Render Nested Components
+
+// TASK: 
+const TypesOfFruit = () => {
+  return (
+    <div>
+      <h2>Fruits:</h2>
+      <ul>
+        <li>Apples</li>
+        <li>Blueberries</li>
+        <li>Strawberries</li>
+        <li>Bananas</li>
+      </ul>
+    </div>
+  );
+};
+
+const Fruits = () => {
+  return (
+    <div>
+      { /* Change code below this line */ }
+        <TypesOfFruit/>
+      { /* Change code above this line */ }
+    </div>
+  );
+};
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        { /* Change code below this line */ }
+          <Fruits/>
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};
+
+
 
 // 11./47
 
