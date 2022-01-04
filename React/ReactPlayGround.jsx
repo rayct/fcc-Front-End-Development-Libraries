051022983516355c8ed26f1b9c139602c1206725
rayct <rayct@icloud.com>
2022-01-04 01:34:38 +0000

    React Course Progress Update - 18/47

--- React/ReactPlayGround.jsx
+++ React/ReactPlayGround.jsx
@@ -370,9 +370,90 @@ class ToDo extends React.Component {
 
 
 
-// 16./47 - 
-// 17./47 - 
-// 18./47 - 
+// 16./47 - Use Default props
+
+// NOTES: React also has an option to set default props.
+// You can assign default props to a component as a property on the component itself and React assigns the default prop if necessary.
+// This allows you to specify what a prop value should be if no value is explicitly provided.
+// For example, if you declare MyComponent.defaultProps = { location: 'San Francisco' }, 
+// you have defined a location prop that's set to the string San Francisco, unless you specify otherwise.
+// React assigns default props if props are undefined, but if you pass null as the value for a prop, it will remain null.
+
+// TASK: The code editor shows a ShoppingCart component.
+// Define default props on this component which specify a prop items with a value of 0.
+const ShoppingCart = (props) => {
+  return (
+    <div>
+      <h1>Shopping Cart Component</h1>
+    </div>
+  )
+};
+// Change code below this line
+ShoppingCart.defaultProps = { items: 0 };
+
+
+
+// 17./47 - Override Default Props
+
+// NOTES: The ability to set default props is a useful feature in React.
+// The way to override the default props is to explicitly set the prop values for a component.
+// Remember that the syntax to add a prop to a component looks similar to how you add HTML attributes.
+// However, since the value for quantity is an integer, it won't go in quotes but it should be wrapped in curly braces.
+// For example, {100}. This syntax tells JSX to interpret the value within the braces directly as JavaScript.
+
+// TASK: The ShoppingCart component now renders a child component Items.
+// This Items component has a default prop quantity set to the integer 0.
+// Override the default prop by passing in a value of 10 for quantity.
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+}
+
+Items.defaultProps = {
+  quantity: 0
+}
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    { /* Change code below this line */ }
+    return <Items quantity={10} />
+    { /* Change code above this line */ }
+  }
+};
+
+
+
+// 18./47 - Use PropTypes to Define the Props You Expect
+
+// NOTES: As of React v15.5.0, PropTypes is imported independently from React, like this: import PropTypes from 'prop-types';
+
+// TASK: Define propTypes for the Items component to require quantity as a prop and verify that it is of type number.
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+};
+
+// Change code below this line
+
+// Change code above this line
+
+Items.defaultProps = {
+  quantity: 0
+};
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <Items />
+  }
+};
+
+
+
+
 // 19./47 - 
 // 20./47 - 
 // 21./47 - 
