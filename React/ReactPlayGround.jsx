053522e715a8847a975c237c63e89c5d4bac9baf
rayct <rayct@icloud.com>
2022-01-09 20:00:40 +0000

    React Course Progress Update - 23/47

--- React/ReactPlayGround.jsx
+++ React/ReactPlayGround.jsx
@@ -634,11 +634,36 @@ class MyComponent extends React.Component {
 
 
 
-// 24./47 - 
+// 24./47 - Set State with this.setState
 
-// TASK: 
+// TASK: There is a button element in the code editor which has an onClick() handler.
+// This handler is triggered when the button receives a click event in the browser, and runs the handleClick method defined on MyComponent.
+// Within the handleClick method, update the component state using this.setState().Set the name property in state to equal the string React Rocks!.
 // NOTES:
-
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Initial State'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    // Change code below this line
+    this.setState({
+      name: 'React Rocks!'
+    });
+    // Change code above this line
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+};
 
 
 // 25./47 - 
