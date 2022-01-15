e7f5511e816af93459e2a9994884c4a637eab6de
rayct <rayct@icloud.com>
2022-01-15 01:45:47 +0000

    React Course: Progress Update: - 31/47

--- React/ReactPlayGround.jsx
+++ React/ReactPlayGround.jsx
@@ -986,10 +986,63 @@ class Navbar extends React.Component {
 
 
 
-// 31./47 - 
+// 31./47 - Pass a Callback as Props
 
 // TASK: 
 // NOTES:
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
+  render() {
+    return (
+       <div>
+        { /* Change code below this line */ }
+          <GetInput name={this.state.name}/>
+        { /* Change code above this line */ }
+       </div>
+    );
+  }
+};
+
+class GetInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Get Input:</h3>
+        <input
+          value={this.props.input}
+          onChange={this.props.handleChange}/>
+      </div>
+    );
+  }
+};
+
+class RenderInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Input Render:</h3>
+        <p>{this.props.input}</p>
+      </div>
+    );
+  }
+};
 
 
 // 32./47 - 
