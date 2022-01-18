5a9b47b6b045a36c3aaa3b935bc470b37ac95f68
rayct <rayct@icloud.com>
2022-01-18 18:38:37 +0000

    React Course: Progress Update: 39/47

--- React/ReactPlayGround.jsx
+++ React/ReactPlayGround.jsx
@@ -1357,12 +1357,45 @@ class MagicEightBall extends React.Component {
   }
 }
 
-// 39./47 - 
+// 39./47 - Render with an If-Else Condition
 
 // TASK:
 //  
 // NOTES:
 // 
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    }
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState((state) => ({
+      display: !state.display
+    }));
+  }
+  render() {
+    // Change code below this line
+    // SOLUTION 1
+    if (this.state.display) {
+      return (
+         <div>
+           <button onClick={this.toggleDisplay}>Toggle Display</button>
+           <h1>Displayed!</h1>
+         </div>
+      );
+    } else {
+      return (
+        <div>
+           <button onClick={this.toggleDisplay}>Toggle Display</button>
+         </div>
+      );
+    }
+  }
+};
+// // SOLUTION 2
 
 
 // 40./47 - 
