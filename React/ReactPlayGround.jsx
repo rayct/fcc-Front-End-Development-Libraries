59c6c07d18b41a8925a15e0b6afdfd93e1666e8f
rayct <rayct@icloud.com>
2022-01-19 00:46:46 +0000

    React Course: Progress Update: - 40/47

--- React/ReactPlayGround.jsx
+++ React/ReactPlayGround.jsx
@@ -1395,16 +1395,36 @@ class MyComponent extends React.Component {
     }
   }
 };
-// // SOLUTION 2
 
-
-// 40./47 - 
+// 40./47 - Use && for a More Concise Conditional
 
 // TASK:
 //  
 // NOTES:
 // 
-
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    }
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState(state => ({
+      display: !state.display
+    }));
+  }
+  render() {
+    // Change code below this line
+    return (
+       <div>
+         <button onClick={this.toggleDisplay}>Toggle Display</button>
+         <h1>Displayed!</h1>
+       </div>
+    );
+  }
+};
 
 // 41./47 - 
 
