501d51cbcb8e8f6505faaad4fbc531ab2dcb3e4c
rayct <rayct@icloud.com>
2022-01-20 02:13:06 +0000

    React Course: Progress Update: - 41/47

--- React/ReactPlayGround.jsx
+++ React/ReactPlayGround.jsx
@@ -1427,12 +1427,84 @@ class MyComponent extends React.Component {
   }
 };
 
-// 41./47 - 
+// 41./47 - Use a Ternary Expression for Conditional Rendering
 
 // TASK:
-//  
-// NOTES:
-// 
+// The code editor has three constants defined within the CheckUserAge component's render() method.
+// They are called buttonOne, buttonTwo, and buttonThree.
+// Each of these is assigned a simple JSX expression representing a button element.
+// First, initialize the state of CheckUserAge with input and userAge both set to values of an empty string.
+
+// Once the component is rendering information to the page, users should have a way to interact with it.
+// Within the component's return statement, set up a ternary expression that implements the following logic: 
+// when the page first loads, render the submit button, buttonOne, to the page.
+// Then, when a user enters their age and clicks the button, render a different button based on the age.
+// If a user enters a number less than 18, render buttonThree.
+// If a user enters a number greater than or equal to 18, render buttonTwo.
+// NOTES: // Using a Nested Ternary Operator
+//
+const inputStyle = {
+  width: 235,
+  margin: 5
+};
+
+class CheckUserAge extends React.Component {
+  constructor(props) {
+    super(props);
+    // Change code below this line
+    this.state = {
+      userAge: '',
+      input: ''
+      }
+    // Change code above this line
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      input: e.target.value,
+      userAge: ''
+    });
+  }
+  submit() {
+    this.setState(state => ({
+      userAge: state.input
+    }));
+  }
+  render() {
+    const buttonOne = <button onClick={this.submit}>Submit</button>;
+    const buttonTwo = <button>You May Enter</button>;
+    const buttonThree = <button>You Shall Not Pass</button>;
+    return (
+      <div>
+        <h3>Enter Your Age to Continue</h3>
+        <input
+          style={inputStyle}
+          type='number'
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+        <br />
+        {/* Change code below this line */}
+        // Using a Nested Ternary Operator
+        // Solution 1
+        {this.state.userAge === ''
+          ? buttonOne
+          : this.state.userAge < 18  
+          ? buttonThree
+          : buttonTwo} // Using a Nested Ternary Operator
+      
+        // Solution 2
+        {this.state.userAge === ''
+            ? buttonOne
+            : this.state.userAge >= 18
+            ? buttonTwo
+            : buttonThree}
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
 
 
 // 42./47 - 
