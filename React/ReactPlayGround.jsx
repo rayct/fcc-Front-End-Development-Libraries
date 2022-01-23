44cf0c4ab8debc09e0d2b900476fb1b7d14d1f22
rayct <rayct@icloud.com>
2022-01-23 18:49:09 +0000

    React Course: Progress Update: - 44/47

--- React/ReactPlayGround.jsx
+++ React/ReactPlayGround.jsx
@@ -1599,13 +1599,69 @@ class GateKeeper extends React.Component {
   }
 };
 
-// 44./47 - 
+// 44./47 - Use Array.map() to Dynamically Render Elements
 
 // TASK:
 //  
 // NOTES:
 // 
+const textAreaStyles = {
+  width: 235,
+  margin: 5
+};
 
+class MyToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    // Change code below this line
+      this.state = {
+        userInput: '',
+        toDoList: []
+      };
+    // Change code above this line
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit() {
+    const itemsArray = this.state.userInput.split(',');
+    this.setState({
+      toDoList: itemsArray
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value
+    });
+  }
+
+  // Solution 1:
+  // render() {
+  //   const items = this.state.toDoList.map(function(element) {
+  //     return <li>{ element }</li>
+  //   }); // Change this line
+
+    // Solution 2:
+    render() {
+    const items = this.state.toDoList.map((item, i) => {
+      return <li key={i}>{item}</li>;
+    }); // Change this line
+      
+    return (
+      <div>
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.userInput}
+          style={textAreaStyles}
+          placeholder='Separate Items With Commas'
+        />
+        <br />
+        <button onClick={this.handleSubmit}>Create List</button>
+        <h1>My "To Do" List:</h1>
+        <ul>{items}</ul>
+      </div>
+    );
+  }
+}
 
 // 45./47 - 
 
