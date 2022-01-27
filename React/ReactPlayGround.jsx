187c00148780da4f697f3b799eb31de1e92efbc7
rayct <rayct@icloud.com>
2022-01-27 02:08:47 +0000

    React Course: Progress Update: - 46/47

--- React/ReactPlayGround.jsx
+++ React/ReactPlayGround.jsx
@@ -1692,7 +1692,7 @@ const frontEndFrameworks = [
 
 function Frameworks() {
   const renderFrameworks = frontEndFrameworks.map(function (element, e) { <li key={e}>{element}</li> }); // Solution 1
-  const renderFrameworks = frontEndFrameworks.map((fw, i) => <li key={i}>{fw}</li>); // Solution 2
+  const renderFrameworks = frontEndFrameworks.map((fw, i) => <li key={i}>{fw}</li>); // Solution 2 - fcc
   const renderFrameworks = frontEndFrameworks.map((element, e) => {
     return <li key={e}>{element}</li>;
   }); // Solution 3
@@ -1711,9 +1711,19 @@ function Frameworks() {
 // 46./47 - Use Array.filter() to Dynamically Filter an Array
 
 // TASK:
-// 
+// In the code editor, MyComponent's state is initialized with an array of users.
+// Some users are online and some aren't. Filter the array so you see only the users who are online.
+// To do this, first use filter to return a new array containing only the users whose online property is true.
+// Then, in the renderOnline variable, map over the filtered array, and return a li element for each user that contains the text of their username.
+// Be sure to include a unique key as well, like in the last challenges.
+
 // NOTES:
-// 
+// The map array method is a powerful tool that you will use often when working with React.
+// Another method related to map is filter, which filters the contents of an array based on a condition,
+// then returns a new array. For example, if you have an array of users that all have a property online which can be set to true or false,
+// you can filter only those users that are online by writing:
+// {/* let onlineUsers = users.filter(user => user.online); */ }
+
 class MyComponent extends React.Component {
   constructor(props) {
     super(props);
@@ -1747,8 +1757,16 @@ class MyComponent extends React.Component {
     };
   }
   render() {
-    const usersOnline = null; // Change this line
-    const renderOnline = null; // Change this line
+
+    // Solution 1
+    const usersOnline = this.state.users.filter(user => {
+      return user.online
+      }); // Change this line
+    const renderOnline = usersOnline.map(user => {
+      return <li key={user.username}>{user.username}</li>;
+    }); // Change this line
+
+    // Solution 1
     return (
       <div>
         <h1>Current Online Users:</h1>
@@ -1758,9 +1776,19 @@ class MyComponent extends React.Component {
   }
 }
 
-// 47./47 - 
+// 47./47 - Render React on the Server with renderToString
 
 // TASK:
 //  
 // NOTES:
-// 
+//
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div/>
+  }
+};
+
+// Change code below this line
