f5bbc3b2bb42f91a387e47b645d680a8ec884a8b
rayct <rayct@icloud.com>
2022-01-28 01:15:20 +0000

    Redux Course: Progress Update: - 1/17

---
+++ Redux/Redux_Coruse.jsx
@@ -0,0 +1,163 @@
+// Redux Course:  1/17 -  Create a Redux Store
+// Notes: 
+// Redux is a state management framework that can be used with a number of different web technologies, including React.
+// In Redux, there is a single state object that's responsible for the entire state of your application.
+// This means if you had a React app with ten components, and each component had its own local state,
+// the entire state of your app would be defined by a single state object housed in the Redux store.
+// This is the first important principle to understand when learning Redux: the Redux store is the single source of truth when it comes to application state.
+
+// Task: 
+// Declare a store variable and assign it to the createStore() method, passing in the reducer as an argument.
+const reducer = (state = 5) => {
+  return state;
+}
+
+// Redux methods are available from a Redux object
+// For example: Redux.createStore()
+// Define the store here:
+const store = Redux.createStore(reducer);
+
+
+
+// Redux Course:  2/17 - 
+// Notes: 
+// 
+
+// Task: 
+// 
+
+
+
+// Redux Course:  3/17 - 
+// Notes: 
+// 
+
+// Task: 
+// 
+
+
+
+// Redux Course:  4/17 - 
+// Notes: 
+// 
+
+// Task:
+// 
+
+
+
+// Redux Course:  5/17 - 
+// Notes: 
+// 
+
+// Task: 
+// 
+
+
+
+// Redux Course:  6/17 - 
+// Notes: 
+// 
+
+// Task: 
+// 
+
+
+
+// Redux Course:  7/17 - 
+// Notes: 
+// 
+
+// Task: 
+// 
+
+
+
+// Redux Course:  8/17 - 
+// Notes: 
+// 
+
+// Task: 
+//
+
+
+
+// Redux Course:  9/17 - 
+// Notes: 
+// 
+
+// Task: 
+//
+
+
+
+// Redux Course:  10/17 - 
+// Notes: 
+// 
+
+// Task: 
+//
+
+
+
+// Redux Course:  11/17 - 
+// Notes: 
+// 
+
+// Task: 
+//
+
+
+
+// Redux Course:  12/17 - 
+// Notes: 
+// 
+
+// Task: 
+//
+
+
+
+// Redux Course:  13/17 - 
+// Notes: 
+// 
+
+// Task: 
+//
+
+
+
+// Redux Course:  14/17 - 
+// Notes: 
+// 
+
+// Task: 
+//
+
+
+
+// Redux Course:  15/17 - 
+// Notes: 
+// 
+
+// Task: 
+//
+
+
+
+// Redux Course:  16/17 - 
+// Notes: 
+// 
+
+// Task: 
+//
+
+
+
+// Redux Course:  17/17 - 
+// Notes: 
+// 
+
+// Task: 
+//
+
