8c3526750e59f01ee84a5d10d3efba33a3e054d8
rayct <rayct@icloud.com>
2022-01-30 01:39:20 +0000

    Redux Course: Progress Update: - 2/17

--- Redux/ReduxPlayGround.jsx
+++ Redux/ReduxPlayGround.jsx
@@ -19,13 +19,20 @@ const store = Redux.createStore(reducer);
 
 
 
-// Redux Course:  2/17 - 
+// Redux Course:  2/17 - Get State from Redux Store
 // Notes: 
-// 
+// The Redux store object provides several methods that allow you to interact with it.
+// For example, you can retrieve the current state held in the Redux store object with the getState() method.
 
 // Task: 
-// 
+// The code from the previous challenge is re-written more concisely in the code editor.
+// Use store.getState() to retrieve the state from the store, and assign this to a new variable currentState.
+const store = Redux.createStore(
+  (state = 5) => state
+);
 
+// Change code below this line
+let currentState = store.getState();
 
 
 // Redux Course:  3/17 - 
