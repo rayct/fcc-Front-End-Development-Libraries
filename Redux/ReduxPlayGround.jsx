801c29c792c2d6b00e4c461716fab5d50339c934
rayct <rayct@icloud.com>
2022-02-03 01:22:45 +0000

    Redux Course: Progress Update: - 11/17

--- Redux/ReduxPlayGround.jsx
+++ Redux/ReduxPlayGround.jsx
@@ -361,13 +361,21 @@ const store = Redux.createStore(rootReducer);
 // 
 
 // Task: 
-//
+// There's a basic notesReducer() and an addNoteText() action creator defined in the code editor.
+// Finish the body of the addNoteText() function so that it returns an action object.
+// The object should include a type property with a value of ADD_NOTE, and also a text property set to the note data that's passed into the action creator.
+// When you call the action creator, you'll pass in specific note information that you can access for the object.
+
+// Next, finish writing the switch statement in the notesReducer().
+// You need to add a case that handles the addNoteText() actions.
+// This case should be triggered whenever there is an action of type ADD_NOTE and it should return the text property on the incoming action as the new state.
 const ADD_NOTE = 'ADD_NOTE';
 
 const notesReducer = (state = 'Initial State', action) => {
   switch(action.type) {
     // Change code below this line
-
+    case ADD_NOTE:
+      return action.text
     // Change code above this line
     default:
       return state;
@@ -376,7 +384,10 @@ const notesReducer = (state = 'Initial State', action) => {
 
 const addNoteText = (note) => {
   // Change code below this line
-
+  return {
+    type: 'ADD_NOTE',
+    text: note
+    };
   // Change code above this line
 };
 
