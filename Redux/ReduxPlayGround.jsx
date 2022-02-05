55c8ecb8a0d724f796be0df1908e9d8ea54edf93
rayct <rayct@icloud.com>
2022-02-05 02:06:38 +0000

    Redux Course: Progress Update: - 14/17

--- Redux/ReduxPlayGround.jsx
+++ Redux/ReduxPlayGround.jsx
@@ -492,13 +492,46 @@ const decAction = () => { // ES6 Style Function declaration
 
 const store = Redux.createStore(counterReducer); // Define the Redux store here, passing in your reducers
 
-// Redux Course:  14/17 - 
+
+
+// Redux Course:  14/17 - Never Mutate State
 // Notes: 
+// These final challenges describe several methods of enforcing the key principle of state immutability in Redux.
+// Immutable state means that you never modify state directly, instead, you return a new copy of state.
 // 
 
 // Task: 
 //
+const ADD_TO_DO = 'ADD_TO_DO';
 
+// A list of strings representing tasks to do:
+const todos = [
+  'Go to the store',
+  'Clean the house',
+  'Cook dinner',
+  'Learn to code',
+];
+
+const immutableReducer = (state = todos, action) => {
+  switch(action.type) {
+    case ADD_TO_DO:
+      // Don't mutate state here or the tests will fail
+      const newList = [...todos];
+      newList.push(action.todo);
+      return newList;
+    default:
+      return state;
+  }
+};
+
+const addToDo = (todo) => {
+  return {
+    type: ADD_TO_DO,
+    todo
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
 
 
 // Redux Course:  15/17 - 
