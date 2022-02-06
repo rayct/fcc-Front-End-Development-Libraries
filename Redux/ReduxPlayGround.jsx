d199e8e26ab1e3586ba7a14ba474e50f66475b27
rayct <rayct@icloud.com>
2022-02-06 15:45:59 +0000

    Redux Course: Progress Update: - 15/17

--- Redux/ReduxPlayGround.jsx
+++ Redux/ReduxPlayGround.jsx
@@ -548,12 +548,12 @@ const store = Redux.createStore(immutableReducer);
 // 
 
 // Task: 
-//
+// Use the spread operator to return a new copy of state when a to-do is added.
 const immutableReducer = (state = ['Do not mutate state!'], action) => {
   switch(action.type) {
     case 'ADD_TO_DO':
       // Don't mutate state here or the tests will fail
-      return [...state, action.todo];
+      return [...state, action.todo]; // Spread Operator used to return new state.
     default:
       return state;
   }
