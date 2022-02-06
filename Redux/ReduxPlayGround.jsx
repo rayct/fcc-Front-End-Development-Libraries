d3ab84ad33584a5cf565ee6c5e5fe97328f84da0
rayct <rayct@icloud.com>
2022-02-06 15:55:17 +0000

    Redux Course: Progress Update: - 16/17

--- Redux/ReduxPlayGround.jsx
+++ Redux/ReduxPlayGround.jsx
@@ -574,12 +574,32 @@ const store = Redux.createStore(immutableReducer);
 
 
 
-// Redux Course:  16/17 - 
+// Redux Course:  16/17 - Remove an Item from an Array
 // Notes: 
-// 
+// Time to practice removing items from an array.
+// The spread operator can be used here as well. Other useful JavaScript methods include slice() and concat().
 
 // Task: 
-//
+// The reducer and action creator were modified to remove an item from an array based on the index of the item. 
+// Finish writing the reducer so a new state array is returned with the item at the specific index removed.
+const immutableReducer = (state = [0,1,2,3,4,5], action) => {
+  switch(action.type) {
+    case 'REMOVE_ITEM':
+      // Don't mutate state here or the tests will fail
+      return
+    default:
+      return state;
+  }
+};
+
+const removeItem = (index) => {
+  return {
+    type: 'REMOVE_ITEM',
+    index
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
 
 
 
