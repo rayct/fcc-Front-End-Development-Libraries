f2de2937ed75b205b5f3daaeaa017bf2c23dcfb2
rayct <rayct@icloud.com>
2022-02-07 15:21:15 +0000

    React and Redux Course: Progress Update: - 1/10

--- ReactAndRedux/ReactReduxPlayGround.jsx
+++ ReactAndRedux/ReactReduxPlayGround.jsx
@@ -8,7 +8,13 @@
 // 
 class DisplayMessages extends React.Component {
   // Change code below this line
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: '',
+            messages: []
+        }
+    }
   // Change code above this line
   render() {
     return <div />
