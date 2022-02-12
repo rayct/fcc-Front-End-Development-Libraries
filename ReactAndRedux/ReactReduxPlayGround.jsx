67fad2cbed9fb618c767bf2c2c1834299864e2a8
rayct <rayct@icloud.com>
2022-02-12 18:24:17 +0000

    React and Redux Course: Progress Update: - 4/10

--- ReactAndRedux/ReactReduxPlayGround.jsx
+++ ReactAndRedux/ReactReduxPlayGround.jsx
@@ -146,7 +146,9 @@ class DisplayMessages extends React.Component {
 // The messageReducer should return the current state if called with any other actions.
 // Define ADD, addMessage(), messageReducer(), and store here:
 
-// THE SCTION TYPE 'ADD' WHICH IS SET TO A CONST ADD
+// NON ES6 WAY: Solution 1
+
+// THE ACTION TYPE 'ADD' WHICH IS SET TO A CONST ADD
 const ADD = 'ADD';
 // FUNCTION THAT WORKS WITH THE REDUCER
  function addMessage(message) {
@@ -167,7 +169,49 @@ const ADD = 'ADD';
 // THE REDUX STORE
 const store = Redux.createStore(messageReducer);
  
-// freeCodeCamp Solution
+// freeCodeCamp: Solution 2
+const ADD = 'ADD';
+
+const addMessage = (message) => { // THE ACTION TYPE 'ADD' WHICH IS SET TO A CONST ADD
+  return {
+    type: ADD,
+    message
+  }
+};
+
+const messageReducer = (state = [], action) => {
+  switch (action.type) { // <= SWITCH STATEMENT
+    case ADD: // <= CASE STATEMENT
+      return [
+        ...state,
+        action.message
+      ];
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(messageReducer); // <= REDUX STORE
+
+
+
+// React and Redux Course:  4/10 - Use Provider to Connect Redux to React
+// Notes: 
+// React Redux is available as a global variable here, so you can access the Provider with dot notation.
+// The code in the editor takes advantage of this and sets it to a constant Provider for you to use in the AppWrapper render method.
+
+// Task:
+// The code editor now shows all your Redux and React code from the past several challenges.
+// It includes the Redux store, actions, and the DisplayMessages component.
+// The only new piece is the AppWrapper component at the bottom.Use this top level component to render the Provider from ReactRedux, and pass the Redux store as a prop.
+// Then render the DisplayMessages component as a child. Once you are finished, you should see your React component rendered to the page.
+
+// The AppWrapper should render.
+// The Provider wrapper component should have a prop of store passed to it, equal to the Redux store.
+// DisplayMessages should render as a child of AppWrapper.
+// The DisplayMessages component should render an h2, input, button, and ul element.
+
+// Redux:
 const ADD = 'ADD';
 
 const addMessage = (message) => {
@@ -189,17 +233,71 @@ const messageReducer = (state = [], action) => {
   }
 };
 
+
+
 const store = Redux.createStore(messageReducer);
 
+// React:
 
+class DisplayMessages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      messages: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  submitMessage() {  
+    this.setState((state) => {
+      const currentMessage = state.input;
+      return {
+        input: '',
+        messages: state.messages.concat(currentMessage)
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        <input
+          value={this.state.input}
+          onChange={this.handleChange}/><br/>
+        <button onClick={this.submitMessage}>Submit</button>
+        <ul>
+          {this.state.messages.map( (message, idx) => {
+              return (
+                 <li key={idx}>{message}</li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+};
 
-// React and Redux Course:  4/10 - 
-// Notes: 
-// 
+const Provider = ReactRedux.Provider;
 
-// Task:
-// 
-
+class AppWrapper extends React.Component {
+  
+  // Render the Provider below this line
+  render() {
+    return (
+      <Provider store={store}>
+        <DisplayMessages />
+      </Provider>
+    )
+  }
+  // Change code above this line
+};
 
 
 
