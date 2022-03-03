aa4bbef160ba819dc0ddd708b0d2dcc1dc23071f
rayct <rayct@icloud.com>
2022-03-03 17:38:30 +0000

    downgraded marked cdn lib to version 0.8.1

--- Simple-React-Markdown-Previewer/index.js
+++ Simple-React-Markdown-Previewer/index.js
@@ -1,11 +1,14 @@
 
 // import * as React from "https://cdn.skypack.dev/react@17.0.1";
 // import * as ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
+// marked.setOptions({
+//     breaks: true,
+// })
 class App extends React.Component {
     state = {
         text: ''
     }
-
+    
     handleChange = (e) => {
         this.setState({
             text: e.target.value
@@ -13,7 +16,7 @@ class App extends React.Component {
     }
     render() {
         const { text } = this.state;
-
+        
         const markdown = marked(text);
 
         return (
@@ -21,12 +24,12 @@ class App extends React.Component {
                 <h2 className="text-center m-4">Convert Markdown</h2>
                 <div className="row">
                     <div className="col-6">
-                        <h6 className='h6-text'>Editor:</h6>
-                        <textarea className='form-control' id='editer' value={text} onChange={this.handleChange} />
+                        <h6 className='h6-text'>Editor</h6>
+                        <textarea className='form-control p-2' id='editor' value={ text } onChange={this.handleChange}/>
                     </div>
                     <div className='col-6' id="preview">
-                    <h6 className='h6-text'>Previewer:</h6>
-                    <div className='preview rounded' dangerouslySetInnerHTML={{__html: markdown}} />
+                    <h6 className='h6-text'>Previewer</h6>
+                        <div className='preview rounded p-2' dangerouslySetInnerHTML={{ __html: markdown }} />
                     </div>
                 </div>
             </div>
@@ -35,3 +38,4 @@ class App extends React.Component {
 }
 
 ReactDOM.render(<App />, document.getElementById('app')); 
+ 
