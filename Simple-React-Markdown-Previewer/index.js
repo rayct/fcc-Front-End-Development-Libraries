0837c7c5d917f88f336e199c187e993c34d09629
rayct <rayct@icloud.com>
2022-03-04 01:59:19 +0000

    All Test Passed - 8/8

--- Simple-React-Markdown-Previewer/index.js
+++ Simple-React-Markdown-Previewer/index.js
@@ -1,12 +1,44 @@
 
 // import * as React from "https://cdn.skypack.dev/react@17.0.1";
 // import * as ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
-marked.setOptions({
-    breaks: true,
-})
+// marked.setOptions({
+//     breaks: true,
+// })
+const initialState = `
+This is a Paragragh
+
+**This is Bolded Text**
+
+> Block Quotes!
+
+# Heading 1
+## Heading 2
+### Heading 3
+#### Heading 4
+- List Item 1
+- List Item 2
+
+[This is My Portfolio](https://www.rayturner.dev/)
+
+This a inline \`<div></div>\`
+
+This is a code block
+\`\`\`
+handleChange = (e) => {
+        this.setState({
+            text: e.target.value
+        })
+    }
+\`\`\`
+
+ This is an Image Item
+
+![JavaScript Image](https://www.freepnglogos.com/uploads/javascript-png/js-logo-on-yellow-11.png)
+`;
+
 class App extends React.Component {
     state = {
-        text: ''
+        text: initialState
     }
     
     handleChange = (e) => {
@@ -17,7 +49,7 @@ class App extends React.Component {
     render() {
         const { text } = this.state;
 
-        const markdown = marked(text);
+        const markdown = marked(text, { breaks: true });
 
         return (
             <div>
@@ -27,9 +59,9 @@ class App extends React.Component {
                         <h6 className='h6-text'>Editor</h6>
                         <textarea className='form-control p-2' id='editor' value={ text } onChange={this.handleChange}/>
                     </div>
-                    <div className='col-6' id="preview">
+                    <div className='col-6'>
                     <h6 className='h6-text'>Previewer</h6>
-                        <div className='preview rounded p-2' dangerouslySetInnerHTML={{ __html: markdown }} />
+                        <div className='preview rounded p-2' dangerouslySetInnerHTML={{ __html: markdown }} id='preview' />
                     </div>
                 </div>
             </div>
