06dee938f334593704ca7f219ebd91a82d321cdf
rayct <rayct@icloud.com>
2022-03-06 18:44:52 +0000

    Changed the property string from url to mp3

--- Drum-Machine/index.js
+++ Drum-Machine/index.js
@@ -3,44 +3,44 @@
 const sounds = [
 {
     key: 'Q',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
 },
 {
     key: 'W',    
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
 },
 {
     key: 'E',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
 },
 {   
     key: 'A',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
 },
 {
     key: 'S',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
 },
 {
     key: 'D', 
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
 },
 {
     key: 'Z', 
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
 },
 {
     key: 'X',   
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
 },
 {
     key: 'C',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
 }];
 
 const App = () => (
     <div id='display' className='display'>
-        <h1>Open HH</h1>
+        <h1>Hit a Drum</h1>
         {sounds.map((sound, idx) => (
             <DrumPad text={sound.key} key={idx} audio={sound.mp3} />
             ))}
@@ -63,31 +63,31 @@ class DrumPad extends React.Component {
     }
     
 
-    playSound = () => {
-        this.audio.current.play();
+playSound = () => {
+    this.audio.current.play();
 
-        const id = this.audio.current.id;
+    const id = this.audio.current.id;
 
-        const parent = this.audio.current.parentNode;
-        parent.classList.add('active');
-        
-        const display = parent.parentNode;
-        display.querySelector('h1').innerText = `${id} is playing`;
-        
-        
-    }
+    const parent = this.audio.current.parentNode;
+    parent.classList.add('active');
+    
+    const display = parent.parentNode;
+    display.querySelector('h1').innerText = `${id} is playing`;
+    
+    
+}
 
 
-    render() {
-        const { text, audio } = this.props;
+render() {
+    const { text, audio } = this.props;
 
-        return (
-            <div className='box' onClick={this.playSound}>
-                {text}
-                <audio ref={this.audio} src={audio} className='clip' id={text} />
-            </div>
-        );
-    }
+    return (
+      <div className="drum-pad" onClick={this.playSound} id={`drum-${text}`}>
+        {text}
+        <audio ref={this.audio} src={audio} className="clip" id={text} />
+      </div>
+    )
+  }
 }
 
 document.addEventListener('keydown', (e) => {
