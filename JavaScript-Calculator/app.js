d299dba34ca7c53ba833b25f59f855fff35f2d1e
rayct <rayct@icloud.com>
2022-04-05 17:29:53 +0100

    Added Pad Buttons and CSS Layout

--- JavaScript-Calculator/app.js
+++ JavaScript-Calculator/app.js
@@ -2,14 +2,15 @@ function App() {
   return (
     <div className="container">
       <div className="grid">
+        <div className="dis"></div>
         <div className="padButton AC">AC</div>
-        <div className="padButton C">C</div>
-        <div className="padButton div">/</div>
-        <div className="padButton times">x</div>
+        <div className="padButton clear">C</div>
+        <div className="padButton divide">/</div>
+        <div className="padButton multiply">x</div>
         <div className="padButton seven">7</div>
         <div className="padButton eight">8</div>
         <div className="padButton nine">9</div>
-        <div className="padButton minus">-</div>
+        <div className="padButton subtract">-</div>
         <div className="padButton four">4</div>
         <div className="padButton five">5</div>
         <div className="padButton six">6</div>
@@ -17,9 +18,9 @@ function App() {
         <div className="padButton one">1</div>
         <div className="padButton two">2</div>
         <div className="padButton three">3</div>
-        <div className="padButton equal">=</div>
+        <div className="padButton equals">=</div>
         <div className="padButton zero">0</div>
-        <div className="padButton dot">.</div>
+        <div className="padButton decimal">.</div>
       </div>
     </div>
   );
