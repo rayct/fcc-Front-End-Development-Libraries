111c62d1df5b33083f10c3c2e5e178b34981b4e9
rayct <rayct@icloud.com>
2022-04-10 01:38:17 +0100

    Started React logic for length Props

--- 25+5-PomodoroClock/app.js
+++ 25+5-PomodoroClock/app.js
@@ -1,10 +1,22 @@
 class App extends React.Component {
   render() {
+    const breakProps = {
+      title: 'Break Length',
+      count: 5,
+      handleDecrease: this.handleDecrease,
+      handleIncrease: this.handleIncrease,
+    };
+    const sessionProps = {
+      title: 'Session Length',
+      count: 25,
+      handleDecrease: this.handleSessionDecrease,
+      handleIncrease: this.handleSessionIncrease,
+    };
     return (
       <div>
         <div className="flex">
-          <SetTimer />
-          <SetTimer />
+          <SetTimer {...breakProps} />
+          <SetTimer {...sessionProps} />
         </div>
         <div>Clock Goes Here</div>
       </div>
@@ -12,15 +24,15 @@ class App extends React.Component {
   }
 }
 
-const SetTimer = () => (
+const SetTimer = (props) => (
   <div className="timer-container">
-    <h1>Break Time</h1>
+    <h1>{props.title}</h1>
     <div className="flex actions-wrapper">
-      <button>
+      <button onClick={props.handleDecrease}>
         <i className="fas fa-minus" />
       </button>
-      <span>5</span>
-      <button>
+      <span>{props.count}</span>
+      <button onClick={props.handleIncrease}>
         <i className="fas fa-plus" />
       </button>
     </div>
